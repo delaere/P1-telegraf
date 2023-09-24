8c5a98be2101790f4eda9ce0e073d684f24f3a69
Christophe Delaere <christophe.delaere@uclouvain.be>
2023-09-24 15:22:24 +0200

    fix import

--- decoder.py
+++ decoder.py
@@ -1,8 +1,7 @@
 import os
 import re
 import yaml
-from datetime import datetime
-from pytz import timezone
+from datetime import datetime, timezone
 
 factors = { 'k':1E3, 'M':1E6, 'G':1E9, 'm':1E-3, 'u':1E-6 }
 
